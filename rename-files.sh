7619006aaa8dd756c15068b8a71a924a85b0a56d
Van-Nostrand <dmdoull43@gmail.com>
2021-11-01 16:50:34 -0700

    update rename files

--- rename-files.sh
+++ rename-files.sh
@@ -6,6 +6,7 @@ read -p "Enter the search text:" search
 # Take the replace text
 read -p "Enter the replace text:" replace
 
+# I have two rename functions on this computer, below references the Arch friendly one
 # rename all files that match with the pattern
-$(rename "s/.$search/.$replace/" *)
+$(renamexm -s/$search/$replace/ *)
 echo "The files are renamed."
